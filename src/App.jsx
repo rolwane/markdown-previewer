0f99c492defa24fd749ebf314c4783796b145e1a
rolwane <rolwane@hotmail.com>
2022-07-21 18:07:46 -0300

    feat: added functionality to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { marked } from 'marked';
+// import Picker from 'emoji-picker-react';
 
 // imported icons
+import { MdOutlineEmojiEmotions } from 'react-icons/md';
 import {
   RiBold,
   RiItalic,
-  RiUnderline,
   RiStrikethrough,
   RiH1,
   RiH2,
@@ -16,7 +17,6 @@ import {
   RiDoubleQuotesR,
   RiListUnordered,
   RiListOrdered,
-  // RiEyeFill,
 } from 'react-icons/ri';
 
 import useLocalStorage from './hooks/useLocalStorage';
@@ -25,10 +25,44 @@ import example from './utils/example';
 // imported components
 import Button from './components/Button';
 
+const buttons = [
+  'bold',
+  'italic',
+  'strikethrough',
+  'H1',
+  'H2',
+  'H3',
+  'link',
+  'table',
+  'code',
+  'blockquote',
+  'unordered list',
+  'ordered list',
+  'emojis',
+];
+
+const buttonIcons = [
+  <RiBold />,
+  <RiItalic />,
+  <RiStrikethrough />,
+  <RiH1 />,
+  <RiH2 />,
+  <RiH3 />,
+  <RiLinksFill />,
+  <RiTable2 />,
+  <RiCodeSSlashLine />,
+  <RiDoubleQuotesR />,
+  <RiListUnordered />,
+  <RiListOrdered />,
+  <MdOutlineEmojiEmotions />,
+];
+
 function App() {
   const [savedCode, setSavedCode] = useLocalStorage('code');
   const [code, setCode] = useState('');
+
   const preview = useRef(null);
+  const textarea = useRef(null);
 
   useEffect(() => {
     if (savedCode) {
@@ -44,25 +78,142 @@ function App() {
     setSavedCode(code);
   }, [code]);
 
+  const getSelectedText = () => {
+    const start = textarea.current.selectionStart;
+    const end = textarea.current.selectionEnd;
+    const selected = code.substring(start, end);
+    const array = code.split('');
+    return [array, start, end, selected];
+  };
+
+  const handleBold = () => {
+    const [array, start, end, selected] = getSelectedText();
+    array.splice(start, end - start, `**${selected}**`);
+    setCode(array.join(''));
+  };
+
+  const handleItalic = () => {
+    const [array, start, end, selected] = getSelectedText();
+    array.splice(start, end - start, `*${selected}*`);
+    setCode(array.join(''));
+  };
+
+  const handleStrike = () => {
+    const [array, start, end, selected] = getSelectedText();
+    array.splice(start, end - start, `~~${selected}~~`);
+    setCode(array.join(''));
+  };
+
+  const handleH1 = () => {
+    const [array, start, end, selected] = getSelectedText();
+    array.splice(start, end - start, `# ${selected}`);
+    setCode(array.join(''));
+  };
+
+  const handleH2 = () => {
+    const [array, start, end, selected] = getSelectedText();
+    array.splice(start, end - start, `## ${selected}`);
+    setCode(array.join(''));
+  };
+
+  const handleH3 = () => {
+    const [array, start, end, selected] = getSelectedText();
+    array.splice(start, end - start, `### ${selected}`);
+    setCode(array.join(''));
+  };
+
+  const handleLink = () => {
+    const [array, start] = getSelectedText();
+    array.splice(start, 0, '[link example](http://example.com)');
+    setCode(array.join(''));
+  };
+
+  const handleTable = () => {
+    const [array, start] = getSelectedText();
+
+    const table = `
+| Heading | Heading |
+|---------|---------|
+| Cell 1  | Cell 1  |
+| Cell 2  | Cell 2  |`;
+
+    array.splice(start, 0, table);
+    setCode(array.join(''));
+  };
+
+  const handleCode = () => {
+    const [array, start, end, selected] = getSelectedText();
+    array.splice(start, end - start, `\`\`\`${selected}\`\`\``);
+    setCode(array.join(''));
+  };
+
+  const handleBlockquote = () => {
+    const [array, start, end, selected] = getSelectedText();
+    array.splice(start, end - start, `> ${selected}`);
+    setCode(array.join(''));
+  };
+
+  const handleUnorderedList = () => {
+    const [array, start, end, selected] = getSelectedText();
+
+    const list = `
+- item 1
+- item 2
+- item 3`;
+
+    if (selected) {
+      array.splice(start, end - start, `- ${selected}`);
+    } else {
+      array.splice(start, end - start, list);
+    }
+    setCode(array.join(''));
+  };
+
+  const handleOrderedList = () => {
+    const [array, start, end, selected] = getSelectedText();
+
+    const list = `
+1. item 1
+2. item 2
+3. item 3`;
+
+    if (selected) {
+      array.splice(start, end - start, `1. ${selected}`);
+    } else {
+      array.splice(start, end - start, list);
+    }
+    setCode(array.join(''));
+  };
+
+  const methods = [
+    handleBold,
+    handleItalic,
+    handleStrike,
+    handleH1,
+    handleH2,
+    handleH3,
+    handleLink,
+    handleTable,
+    handleCode,
+    handleBlockquote,
+    handleUnorderedList,
+    handleOrderedList,
+    handleH3,
+  ];
+
   return (
     <div className="flex overflow-hidden flex-wrap">
 
       <div className="md:w-1/2 h-screen border-r-2 w-full">
-
-        <div className="bg-slate-50 p-2 flex gap-2 border-[#ddd] border-b-[1px] mb-6 flex-wrap">
-          <Button title="bold" startIcon={<RiBold />} />
-          <Button title="italic" startIcon={<RiItalic />} />
-          <Button title="underline" startIcon={<RiUnderline />} />
-          <Button title="strikethrough" startIcon={<RiStrikethrough />} />
-          <Button title="strikethrough" startIcon={<RiH1 />} />
-          <Button title="strikethrough" startIcon={<RiH2 />} />
-          <Button title="strikethrough" startIcon={<RiH3 />} />
-          <Button title="strikethrough" startIcon={<RiLinksFill />} />
-          <Button title="strikethrough" startIcon={<RiTable2 />} />
-          <Button title="strikethrough" startIcon={<RiCodeSSlashLine />} />
-          <Button title="strikethrough" startIcon={<RiDoubleQuotesR />} />
-          <Button title="strikethrough" startIcon={<RiListUnordered />} />
-          <Button title="strikethrough" startIcon={<RiListOrdered />} />
+        <div className="bg-slate-50 p-2 flex gap-2 border-[#ddd] border-b-[1px] mb-6 flex-wrap items-center justify-center">
+          {buttons.map((button, index) => (
+            <Button
+              title={button}
+              startIcon={buttonIcons[index]}
+              onClick={methods[index]}
+              key={button}
+            />
+          ))}
         </div>
 
         <textarea
@@ -71,6 +222,7 @@ function App() {
           onChange={({ target }) => setCode(target.value)}
           placeholder="Write markdown code..."
           value={code}
+          ref={textarea}
         />
       </div>
 
@@ -78,6 +230,7 @@ function App() {
         className="md:w-1/2 w-full p-4 pb-[100px] h-screen markdown-body overflow-y-scroll bg-white border-t-4 md:border-none"
         ref={preview}
       />
+
     </div>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 function Button(props) {
-  const { children, startIcon, title } = props;
+  const {
+    children, startIcon, title, onClick,
+  } = props;
 
   return (
     <button
       type="button"
       className="flex items-center justify-center p-2 text-xl text-slate-800 hover:text-blue-400"
       title={title}
+      onClick={onClick}
       // style={{ border: '1px solid #ddd' }}
     >
       {startIcon}
